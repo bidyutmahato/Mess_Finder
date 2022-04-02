a3628cdc2d47293a0a64838e15521a151ce72801
bidyutmahato <mbiddut45@gmail.com>
2022-04-02 12:29:54 +0530

    MessFinder

---
+++ app/src/main/java/com/bidyut/messfinder/LoginActivity.java
@@ -0,0 +1,43 @@
+package com.bidyut.messfinder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+    TextView signUp;
+    TextView skip;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        getSupportActionBar().hide();
+
+        signUp = findViewById(R.id.btn_signUp);
+        skip = findViewById(R.id.btn_skip);
+
+        // signup here text button
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this,SignUpActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        // for skip button here you click and redirect in home page
+        skip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
