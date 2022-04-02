5a11213088b40be50fcde97064d1f17d02bcfef5
bidyutmahato <mbiddut45@gmail.com>
2022-04-02 23:01:06 +0530

    added menu

--- app/src/main/java/com/bidyut/messfinder/MainActivity.java
+++ app/src/main/java/com/bidyut/messfinder/MainActivity.java
@@ -1,14 +1,61 @@
 package com.bidyut.messfinder;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+BottomNavigationView bnView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bnView = findViewById(R.id.bnView);
+
+        bnView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                int id = item.getItemId();
+
+                if (id==R.id.nav_Home){
+                    loadFrag(new Home(),false);
+
+                }else if (id==R.id.nav_search){
+                    loadFrag(new Search(),false);
+
+                }else if (id==R.id.nav_add){
+                    loadFrag(new Add(),false);
+
+                }else if (id==R.id.nav_manager){
+                    loadFrag(new Manager(),false);
+
+                }else {  // profile
+                    loadFrag(new Profile(),false);
+                }
+
+                return true;
+            }
+        });
+        bnView.setSelectedItemId(R.id.nav_Home);
+    }
+    public void loadFrag (Fragment fragment, boolean flag){
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+
+        if (flag)
+            ft.add(R.id.container,fragment);
+        else
+            ft.replace(R.id.container,fragment);
+        ft.commit();
     }
 }
