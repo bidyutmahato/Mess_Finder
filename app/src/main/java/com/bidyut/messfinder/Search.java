1fbb99e1da8109a4508af45c24d3fe6393c18baa
bidyutmahato <mbiddut45@gmail.com>
2022-05-02 23:30:47 +0530

    version 1.0.4 (search)

--- app/src/main/java/com/bidyut/messfinder/Search.java
+++ app/src/main/java/com/bidyut/messfinder/Search.java
@@ -1,9 +1,12 @@
 package com.bidyut.messfinder;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.view.MenuItemCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -29,6 +32,8 @@ import java.util.ArrayList;
 
 
 public class Search extends Fragment {
+    MenuItem menuItem;
+    SearchView searchView;
 
 
     public Search() {
@@ -57,6 +62,48 @@ public class Search extends Fragment {
         recyclerView.setAdapter(mainAdapter);
 
         return view;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.search,menu);
+        menuItem = menu.findItem(R.id.search);
+        searchView = (SearchView) MenuItemCompat.getActionView(menuItem);
+        searchView.setIconified(true);
+
+        SearchManager searchManager = (SearchManager)getActivity().getSystemService(Context.SEARCH_SERVICE);
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getActivity().getComponentName()));
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                mySearch(s);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                mySearch(s);
+                return false;
+            }
+        });
+
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    private void mySearch(String s) {
+        FirebaseRecyclerOptions<MainModel> options =
+                new FirebaseRecyclerOptions.Builder<MainModel>()
+                        .setQuery(FirebaseDatabase.getInstance().getReference().child("Mess").orderByChild("location").startAt(s).endAt(s+"~"),MainModel.class)
+                        .build();
+        mainAdapter = new MainAdapter(options);
+        mainAdapter.startListening();
+        recyclerView.setAdapter(mainAdapter);
     }
 
     @Override
