13f455fbc6c02507dc516318be917aedcbbe8028
bidyutmahato <mbiddut45@gmail.com>
2022-05-12 10:33:31 +0530

    version 1.1.2 icon update

--- app/src/main/java/com/bidyut/messfinder/SplashActivity.java
+++ app/src/main/java/com/bidyut/messfinder/SplashActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -12,6 +14,10 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_splash);
         getSupportActionBar().hide();
 
@@ -22,6 +28,6 @@ public class SplashActivity extends AppCompatActivity {
                 startActivity(intent);
                 finish();
             }
-        },100);
+        },2650);
     }
 }
